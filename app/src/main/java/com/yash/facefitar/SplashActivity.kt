cf47c60c7acaf76a934fcfd292b0c0b7c5fc4fe2
Yash Raj <ryash5726@gmail.com>
2026-04-03 20:43:08 +0530

    update some files

--- app/src/main/java/com/yash/facefitar/SplashActivity.kt
+++ app/src/main/java/com/yash/facefitar/SplashActivity.kt
@@ -1,20 +1,19 @@
 package com.yash.facefitar
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        // Set theme (optional if using style in Manifest)
         setContentView(R.layout.activity_splash)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        // Delay k baad main activity start karo
+        window.decorView.postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 2000) // 2 seconds
     }
 }
